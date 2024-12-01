8b9ea138b610aaf7d838988551619c1a6057fefb
mik-rzo <michael.panong3@gmail.com>
2024-12-01 09:38:09 +0000

    removed code in main function

--- main.go
+++ main.go
@@ -4,13 +4,7 @@ import (
 	"github.com/teris-io/shortid"
 )
 
-func main() {
-	beeMail := App{make([]*User, 0)}
-
-	mike := User{generateID(), "mike", make([]*Message, 0), nil}
-
-	mike.login(&beeMail)
-}
+func main() {}
 
 func generateID() string {
 	return shortid.MustGenerate()
