11dbd8219197de00115a0230744682a1da8788c9
mik-rzo <michael.panong3@gmail.com>
2024-12-01 20:18:14 +0000

    added tests for sending messages

--- user_test.go
+++ user_test.go
@@ -13,3 +13,23 @@ func TestLogin(t *testing.T) {
 
 	assert.NotEmpty(t, mike.app, "User should have an instance of App.")
 }
+
+func TestSendMessage(t *testing.T) {
+	assert := assert.New(t)
+	mike := User{generateID(), "mike", make([]*Message, 0), nil}
+	charlie := User{generateID(), "charlie", make([]*Message, 0), nil}
+	beeMail := App{make([]*User, 0)}
+	mike.login(&beeMail)
+	charlie.login(&beeMail)
+
+	mike.sendMessage(charlie.id, "hello charlie")
+	message := charlie.inbox[0]
+
+	assert.NotNil(message.id, "Message should have an id.")
+	assert.NotNil(message.timestamp, "Message should have an timestamp.")
+	assert.Equal("hello charlie", message.content, "Message content should match 'hello charlie'.")
+	assert.Equal(&mike, message.from, "Message should be from 'mike'.")
+	assert.Equal(&charlie, message.to, "Message should be to 'charlie'.")
+	assert.Equal(true, message.delivered, "Message should be delivered.")
+	assert.Equal(false, message.read, "Message should not be read.")
+}
