1d679a5c7878876ce83ba9f04e71503443b1d829
mik-rzo <michael.panong3@gmail.com>
2024-12-19 11:34:51 +0000

    added entry point for program execution

--- main/main.go
+++ main/main.go
@@ -4,6 +4,26 @@ import (
 	"github.com/teris-io/shortid"
 )
 
+func main() {
+	beeMail := App{make([]*User, 0)}
+
+	usernames := []string{"mike", "charlie", "meron", "damian", "james", "pablo", "hala", "anna", "john"}
+	var users []*User = nil
+	for _, username := range usernames {
+		user := User{GenerateID(), username, make([]*Message, 0), nil}
+		user.login(&beeMail)
+		users = append(users, &user)
+	}
+
+	mike := users[0]
+	charlie := users[1]
+
+	mike.sendMessage(charlie.id, "hello charlie")
+
+	message := charlie.inbox[0]
+	charlie.readMessage(message.id)
+}
+
 func GenerateID() string {
 	return shortid.MustGenerate()
 }
